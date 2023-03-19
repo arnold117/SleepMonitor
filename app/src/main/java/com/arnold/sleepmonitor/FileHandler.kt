1514ec62761a15ae42b09189ef3fc13f3bf71303
Arnold <arnoldchow@outlook.com>
2023-03-19 16:45:00 +0800

    update FileHandler.kt file read and save works functionally

--- app/src/main/java/com/arnold/sleepmonitor/FileHandler.kt
+++ app/src/main/java/com/arnold/sleepmonitor/FileHandler.kt
@@ -1,23 +1,38 @@
 package com.arnold.sleepmonitor
 
 import android.util.Log
-import com.arnold.sleepmonitor.MApplication.Companion.context
 import java.io.File
 
 class FileHandler{
-    fun listFiles() : Array<out String>? {
-        return context.fileList()
+    private val context = MApplication.context
+    fun checkFolder(folderName: String) {
+        val folder = File(context.getExternalFilesDir(null), folderName)
+        if (!folder.exists()) {
+            Log.d("FileHandler", "Creating folder: $folderName")
+            folder.mkdir()
+        } else {
+            Log.d("FileHandler", "Folder already exists: $folderName")
+        }
     }
 
-    fun readData(identifier: String) : String {
-        val filename = "recording-$identifier"
-        Log.d("FileHandler", "Reading file: $filename")
-        return File(context.filesDir, filename).readText()
+    fun listFiles(folderName: String) : Array<out File>? {
+        val folder = File(context.getExternalFilesDir(null), folderName)
+        return folder.listFiles()
     }
 
-    fun saveData(data: String, identifier: String) {
-        val filename = "recording-$identifier"
-        Log.d("FileHandler", "Saving to file: $filename")
-        File(context.filesDir, filename).writeText(data)
+    fun readData(folderName: String, identifier: String) : String {
+        val filename = "$folderName-$identifier"
+        val path = File(context.getExternalFilesDir(null), folderName)
+
+        Log.d("FileHandler", "Reading file: $folderName/$filename")
+        return File(path, filename).readText()
+    }
+
+    fun saveData(folderName: String, identifier: String, data: String) {
+        val filename = "$folderName-$identifier"
+        val path = File(context.getExternalFilesDir(null), folderName)
+
+        Log.d("FileHandler", "Saving to file: $folderName/$filename")
+        File(path, filename).writeText(data)
     }
 }
