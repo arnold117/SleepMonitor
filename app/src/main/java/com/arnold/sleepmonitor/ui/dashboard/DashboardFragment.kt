c3c82e46ea6b56feefa0c49b1f17d9adf0b50510
Arnold <arnoldchow@outlook.com>
2023-03-19 16:45:26 +0800

    update test for files

--- app/src/main/java/com/arnold/sleepmonitor/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/arnold/sleepmonitor/ui/dashboard/DashboardFragment.kt
@@ -1,12 +1,16 @@
 package com.arnold.sleepmonitor.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.arnold.sleepmonitor.FileHandler
 import com.arnold.sleepmonitor.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -16,6 +20,8 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var fileHandler = FileHandler()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -32,6 +38,26 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val editText: EditText = binding.editText
+
+        val buttonRead: Button = binding.buttonRead
+        buttonRead.setOnClickListener() {
+            Log.d("DashboardFragment", "Read Button clicked")
+            textView.text = "Read Button clicked"
+
+            editText.setText(fileHandler.readData("test", "test"))
+        }
+
+        val buttonWrite: Button = binding.buttonWrite
+        buttonWrite.setOnClickListener() {
+            Log.d("DashboardFragment", "Write Button clicked")
+            textView.text = "Write Button clicked"
+
+            fileHandler.checkFolder("test")
+            fileHandler.saveData("test", "test", editText.text.toString())
+        }
+
         return root
     }
 
