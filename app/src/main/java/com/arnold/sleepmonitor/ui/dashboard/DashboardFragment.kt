b1f99572a28cf489fe4b8f0eab5a149e41da4164
Arnold <arnoldchow@outlook.com>
2023-04-12 15:03:35 +0800

    add listview in dashboard todo: set each an activity.

--- app/src/main/java/com/arnold/sleepmonitor/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/arnold/sleepmonitor/ui/dashboard/DashboardFragment.kt
@@ -5,12 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.arnold.sleepmonitor.FileHandler
+import com.arnold.sleepmonitor.R
 import com.arnold.sleepmonitor.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -34,28 +37,16 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        val editText: EditText = binding.editText
-
-        val buttonRead: Button = binding.buttonRead
-        buttonRead.setOnClickListener() {
-            Log.d("DashboardFragment", "Read Button clicked")
-            textView.text = "Read Button clicked"
-
-            editText.setText(fileHandler.readData("test", "test"))
-        }
-
-        val buttonWrite: Button = binding.buttonWrite
-        buttonWrite.setOnClickListener() {
-            Log.d("DashboardFragment", "Write Button clicked")
-            textView.text = "Write Button clicked"
-
-            fileHandler.checkFolder("test")
-            fileHandler.saveData("test", "test", editText.text.toString())
+        val listView = binding.dashListView
+        val dash = resources.getStringArray(R.array.dash)
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            dash)
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
         }
 
         return root
