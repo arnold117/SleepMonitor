2f4c09158a5c6fbf7d3d472bf996a4d01ad0ef25
Arnold <arnoldchow@outlook.com>
2023-04-22 15:49:07 +0800

    simply add a button to home

--- app/src/main/java/com/arnold/sleepmonitor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/arnold/sleepmonitor/ui/home/HomeFragment.kt
@@ -2,18 +2,10 @@ package com.arnold.sleepmonitor.ui.home
 
 import android.annotation.SuppressLint
 import android.os.*
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.ViewModelProvider
-import com.arnold.sleepmonitor.R
 import com.arnold.sleepmonitor.databinding.FragmentHomeBinding
 import com.arnold.sleepmonitor.recorder.*
 
@@ -27,23 +19,13 @@ class HomeFragment : Fragment() {
     val TAG = "HomeFragment"
 
     @SuppressLint("CommitTransaction")
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-
-        homeViewModel.homeText.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
 
         return root
     }
