6d902fafe577687f504e45935cbfaa0eb9da4c62
Arnold <arnoldchow@outlook.com>
2023-04-25 12:40:37 +0800

    fill data to line chart

--- app/src/main/java/com/arnold/sleepmonitor/ui/records/fragments/DayFragment.kt
+++ app/src/main/java/com/arnold/sleepmonitor/ui/records/fragments/DayFragment.kt
@@ -6,10 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.arnold.sleepmonitor.databinding.FragmentDayBinding
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.ValueFormatter
 
 class DayFragment : Fragment() {
     private var _binding: FragmentDayBinding? = null
@@ -22,12 +23,93 @@ class DayFragment : Fragment() {
         _binding = FragmentDayBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val dayBar = binding.dayBarChart
-        val dayPie = binding.dayPieChart
-
+        setDayLineChart()
         setDayPieChart()
 
         return root
+    }
+
+    private fun setDayLineChart(line: LineChart = binding.dayLineChart) {
+        val xValueList = ArrayList<String>()
+        val yValueList = ArrayList<Float>()
+        val yValueLabel = ArrayList<String>()
+
+        xValueList.add("22:00")
+        xValueList.add("23:00")
+        xValueList.add("00:00")
+        xValueList.add("01:00")
+        xValueList.add("02:00")
+        xValueList.add("03:00")
+        xValueList.add("04:00")
+        xValueList.add("05:00")
+        xValueList.add("06:00")
+        xValueList.add("07:00")
+        xValueList.add("08:00")
+
+        yValueList.add(2f)
+        yValueList.add(1f)
+        yValueList.add(0f)
+        yValueList.add(0f)
+        yValueList.add(1f)
+        yValueList.add(1f)
+        yValueList.add(0f)
+        yValueList.add(0f)
+        yValueList.add(1f)
+        yValueList.add(2f)
+
+        yValueLabel.add("Deep")
+        yValueLabel.add("Light")
+        yValueLabel.add("Awake")
+
+        val lineChartEntry = ArrayList<Entry>()
+        for ((i, value) in yValueList.withIndex()) {
+            lineChartEntry.add(Entry(i.toFloat(), value))
+        }
+
+        val lineDataSet = LineDataSet(lineChartEntry, "Sleep")
+        lineDataSet.setDrawCircles(false)
+        lineDataSet.setDrawValues(false)
+        lineDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        lineDataSet.lineWidth = 3f
+        lineDataSet.color = resources.getColor(android.R.color.holo_blue_light)
+
+        val lineData = LineData(lineDataSet)
+        line.data = lineData
+
+        line.description.isEnabled = false
+        line.setDrawGridBackground(false)
+        line.setDrawBorders(false)
+        line.setTouchEnabled(false)
+
+        line.xAxis.apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    return xValueList[value.toInt()]
+                }
+            }
+        }
+
+        line.axisLeft.apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            setLabelCount(3, true)
+            valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    return yValueLabel[value.toInt()]
+                }
+            }
+        }
+
+        line.axisRight.apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            setDrawLabels(false)
+        }
+
+        line.invalidate()
     }
 
     private fun setDayPieChart(pie: PieChart = binding.dayPieChart) {
@@ -61,6 +143,7 @@ class DayFragment : Fragment() {
 
         pie.description.isEnabled = false
         pie.legend.isEnabled = false
+        pie.setTouchEnabled(false)
 
         pie.invalidate()
     }
