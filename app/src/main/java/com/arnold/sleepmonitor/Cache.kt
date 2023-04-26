3efa75572983d6285965ec7a8d17f31f3f0b898b
Arnold <arnoldchow@outlook.com>
2023-04-26 17:19:03 +0800

    add 2 score calculator

--- app/src/main/java/com/arnold/sleepmonitor/Cache.kt
+++ app/src/main/java/com/arnold/sleepmonitor/Cache.kt
@@ -4,4 +4,18 @@ import java.time.format.DateTimeFormatter
 
 object Cache {
     var isRecording = false
+
+    val stdDurationLow = 360
+    val stdDurationHigh = 600
+
+    val stdDeepRatioLow = 0.2
+    val stdDeepRatioHigh = 0.6
+
+    val stdLightRatioHigh = 0.55
+
+    val stdAwakeHigh = 2
+    val stdSnoreHighPerHour = 1
+
+    val stdLuxHigh = 5.0
+    val stdVolHigh = 32.0
 }
--- app/src/main/java/com/arnold/sleepmonitor/process/Calculator.kt
+++ app/src/main/java/com/arnold/sleepmonitor/process/Calculator.kt
@@ -1,5 +1,6 @@
 package com.arnold.sleepmonitor.process
 
+import android.util.Log
 import com.arnold.sleepmonitor.Cache
 import com.arnold.sleepmonitor.data_structure.SingleTimeData
 import com.arnold.sleepmonitor.data_structure.SingleUnitData
@@ -31,16 +32,52 @@ class Calculator {
     }
 
     fun environmentScore(list: List<SingleUnitData>): Int {
+        val meanLux = list.map { it.meanLux }.average()
+        val meanVolume = list.map { it.meanEnvironmentVolume }.average()
 
-        return 0
+        val divLight = (meanLux - Cache.stdLuxHigh) / Cache.stdLuxHigh
+        val divVolume = (meanVolume - Cache.stdVolHigh) / Cache.stdVolHigh
+
+        val score = (0.5 - divLight) * 50 + (0.5 - divVolume) * 50
+        if (score >= 100) {
+            return 100
+        } else if (score <= 0) {
+            return 0
+        }
+
+        return score.toInt()
     }
 
     fun deepContinuesScore(list: List<SingleUnitData>): Int {
+
+
         return 0
     }
 
     fun respirationQualityScore(list: List<SingleUnitData>): Int {
-        return 0
+        val calculator = Calculator()
+
+        val snoreCount = list.map { it.snoreCount }.sum()
+        val duration = calculator.duration(list[0].time, list[list.size - 1].time)
+
+        val hour = duration / 60
+        val snoreHigh = Cache.stdSnoreHighPerHour * hour
+        val div : Double = (snoreCount.toDouble() - snoreHigh) / snoreHigh
+
+        Log.d("snoreCount", snoreCount.toString())
+        Log.d("duration", duration.toString())
+        Log.d("hour", hour.toString())
+        Log.d("snoreHigh", snoreHigh.toString())
+        Log.d("div", div.toString())
+
+        val score = (0.5 - div) * 100
+        if (score >= 100) {
+            return 100
+        } else if (score <= 0) {
+            return 0
+        }
+
+        return score.toInt()
     }
 
     fun sleepScore(list: List<SingleUnitData>): Int {
